c10dfbaa564bac883231357b7bd77dd4786805dd
haidermonster <150531879+haidermonster@users.noreply.github.com>
2023-11-13 07:23:17 -0800

    Cv
    My Develper Cv

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cv from './Cv';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<Cv/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Cv.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+function Cv() {
+  return (
+    <>
+          <div className="container-fluid" style={{ height: "900px",overflow: "hidden"}}>
+        <div className='row'>
+
+
+          <div className="col-md-6 pt-1 pb-5" style={{ backgroundColor: "rgb(0,92,133)",width: "30%" }}>
+ 
+          <div className='row'> <img src="profile.jpeg" alt="" className='profile mx-auto' /> </div>
+
+<div className='row text-center'><h2 style={{ color: "white"}}>HaiderALI</h2></div>
+<div className='row pb-5'> <hr className='line'/></div>
+<div className='row text-center'> <h5 style={{ color: "white" }}>Contact</h5></div>
+<div className='row mt-1'> <hr className='line mx-auto' /></div>
+<div className='row mt-1'> <p className='icon'><i class='bx bxs-envelope' style={{ color: "white" }} ></i>haider288ai@gmail.com</p></div>
+
+<div className='row mt-1'> <p className='icon2'><i class='bx bxs-phone-call'></i>03202639855</p></div>
+
+<div className='row mt-1'> <p className='icon3'><i class='bx bxl-github' ></i>https://github.com/haidermonster</p></div>
+
+
+<div className='row mt-1'> <p className='icon4'><i class='bx bxl-linkedin-square' ></i>www.linkedin.com/in/haider-ali-89b7242</p></div>
+
+<div className='row text-center'> <h5 style={{ color: "white" }}>SKILLS</h5></div>
+<div className='row mt-1'> <hr className='line mx-auto' /></div>
+
+<div className='row'><h5 className='skill'>HTML</h5></div>
+<div className='row mt-1'>
+  <div className="progress g-0" style={{ width: "100%", height: "10px" }} role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+    <div className="progress-bar" style={{ width: "100%" }}></div>
+  </div>
+</div>
+
+<div className='row'><h5 className='skill2'>CSS</h5></div>
+<div className='row mt-1'>
+  <div className="progress g-0" style={{ width: "100%", height: "10px" }} role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+    <div className="progress-bar" style={{ width: "100%" }}></div>
+  </div>
+</div>
+
+<div className='row'><h5 className='skill3'>JAVASCRIPT</h5></div>
+<div className='row mt-1'>
+  <div className="progress g-0" style={{ width: "100%", height: "10px" }} role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+    <div className="progress-bar" style={{ width: "100%" }}></div>
+  </div>
+</div>
+
+<div className='row'><h5 className='skill4'>REACT</h5></div>
+<div className='row mt-1'>
+  <div className="progress g-0" style={{ width: "100%", height: "10px" }} role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+    <div className="progress-bar" style={{ width: "80%" }}></div>
+  </div>
+</div>
+
+          </div>
+
+
+          <div className='col-md-6' style={{ width: "70%" }}>
+            <div className='row pt-5'><h5>Summary</h5></div>
+            <div className='row mt-1'> <hr className='line2'/></div>
+            <div className='row mt-1'> 
+            <ul className='list'>
+              <li>Self-independent, reliable and friendly individual who work hard to achieve his goals</li>
+              
+              <li>Adaptable quickly,and organized well. Interested in learning the latest web & software technologies quickly</li>
+              
+              <li>Able to work well in teams as well as indvidually.My future goal is to become a senior full-stac developer.</li>
+            </ul>
+            </div>
+
+
+
+            <div className='row pt-5'><h5>Experience</h5></div>
+            <div className='row mt-1'> <hr className='line2'/></div>
+            <div className='row mt-1'> 
+            <ul className='list'>
+              <li>Self-independent, reliable and friendly individual who work hard to achieve his goals</li>
+              
+              <li>Adaptable quickly,and organized well. Interested in learning the latest web & software technologies quickly</li>
+              
+              <li>Able to work well in teams as well as indvidually.My future goal is to become a senior full-stac developer.</li>
+            </ul>
+            </div>
+
+
+          </div>
+
+
+        </div>
+      </div>
+    
+    </>
+  )
+}
+
+export default Cv
